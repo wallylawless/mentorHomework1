ebe9c2d474feca56c2d809f1b2bad93b6552772c
root <root@ubuntu12.(none)>
2013-01-17 17:39:12 -0500

    The first round of tests are passing.

--- tests/WidgetRepositoryTest.php
+++ tests/WidgetRepositoryTest.php
@@ -1,7 +1,27 @@
 <?php
 require_once('../WidgetRepository.php');
 
-class WidgetRepositoryTest implements PHPUnit_Framework_Testcase
+class WidgetRepositoryTest extends PHPUnit_Framework_Testcase
 {
-	$dataModel = $this->getMock('WidgetDataInterface');
+	public function testEmptyInterface()
+	{
+		$dataModel = $this->getMock('WidgetDataInterface', array('getAll', 'getByName', 'getById', 'saveWidget'));
+		$repository = new WidgetRepository($dataModel);
+
+		$this->assertFalse($repository->getAllWidgets(), "Repository should return false when there are no widgets to get");
+	}
+
+	public function testRepositoryReturnsWidgetThroughInterface()
+	{
+		$dataModel = $this->getMock('WidgetDataInterface', array('getAll'));
+
+		$dataModel->expects($this->once())
+				  ->method('getAll')
+				  ->will($this->returnValue(array(array('id'=>1, 'name'=>'First Widget', 'value'=>42))));
+
+		$repository = new WidgetRepository($dataModel);
+		$foundWidgets = $repository->getAllWidgets();
+
+		$this->assertEquals(count($foundWidgets), 1, "The repository is not returning the single widget in the data source");
+	}
 }
